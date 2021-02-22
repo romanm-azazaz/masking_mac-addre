b7b5a82d47438223b9816178d7376f7ddcfbd3ec
Роман Макаров <romanmakarov@MacBook.local>
2021-02-22 19:08:54 +0300

    Hello world!!

--- switch_mac-address.py
+++ switch_mac-address.py
@@ -5,25 +5,27 @@ import uuid
 
 class mac_address:
 
-    @staticmethod
-    def get_defolt():
-        defolt_mac = formatting_mac(hex(uuid.getnode()).replace('0x', ''))
+    @classmethod
+    def get_defolt(cls):
+        defolt_mac = cls.formatting_mac(hex(uuid.getnode()).replace('0x', ''))
         return defolt_mac
 
-    @staticmethod
-    def generation():
-        substitute_mac = formatting_mac(uuid.uuid4().hex[0:12])
+    @classmethod
+    def generation(cls):
+        substitute_mac = cls.formatting_mac(uuid.uuid4().hex[0:12])
         return substitute_mac
+
+    @classmethod
+    def formatting_mac(cls, mac_hex):
+       mac = ':'.join(mac_hex[i: i + 2] for i in range(0, 11, 2))
+       return mac
 
     @staticmethod
     def switch():
         cmd = F'ifconfig #{interface} ether #{mac_address}'
 
-def formatting_mac(mac_hex):
-       mac = ':'.join(mac_hex[i: i + 2] for i in range(0, 11, 2))
-       return mac
 
 defolt_mac = mac_address.get_defolt()
 substitute_mac = mac_address.generation()
 
-print(F'defolt_mac {DEFOLT_MAC}\nsubstitute_mac {substitute_mac}')
+print(F'defolt_mac {defolt_mac}\nsubstitute_mac {substitute_mac}')
