fc39da56f4722163ad99a72556c93fb821e89e65
Роман Макаров <romanmakarov@MacBook.local>
2021-02-23 20:07:16 +0300

    added argparse

--- switch_mac-address.py
+++ switch_mac-address.py
@@ -1,19 +1,19 @@
 import subprocess
 import argparse
-import random
 import uuid
+import sys
 
 class mac_address:
 
     @classmethod
-    def get_defolt(cls):
-        defolt_mac = cls.formatting_mac(hex(uuid.getnode()).replace('0x', ''))
-        return defolt_mac
+    def get_default(cls):
+        default_mac = cls.formatting_mac(hex(uuid.getnode()).replace('0x', ''))
+        return default_mac
 
     @classmethod
-    def generation(cls):
-        substitute_mac = cls.formatting_mac(uuid.uuid4().hex[0:12])
-        return substitute_mac
+    def get_fake(cls):
+        fake_mac = cls.formatting_mac(uuid.uuid4().hex[0:12])
+        return fake_mac
 
     @classmethod
     def formatting_mac(cls, mac_hex):
@@ -21,11 +21,23 @@ class mac_address:
        return mac
 
     @classmethod
-    def switch(cls):
-        cmd = F'ifconfig #{interface} ether #{cls.generation}'
+    def call_switch(cls):
+        interface = 'en0'
+        print(F'default mac {cls.get_default()}\nfake mac {cls.get_fake()}')
+        cmd = F'ifconfig {interface} ether {cls.get_fake()}'        
+        print(cmd)
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--random', action='store_const', const=True, default=False,
+                        help='Generates a random set of characters similar to mac-faddres')
+    script_key = parser.parse_args()
+    return script_key.random
 
-defolt_mac = mac_address.get_defolt()
-substitute_mac = mac_address.generation()
+def main():
+    if create_parser(): 
+        mac_address.call_switch()
+    else: sys.exit(1)
 
-print(F'defolt_mac {defolt_mac}\nsubstitute_mac {substitute_mac}')
+if __name__ == '__main__': main()
+
